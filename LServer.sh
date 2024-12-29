17b4c21ab0eb3cec01a57d37a10e21ea846135d4
William <126926699+GitXpresso@users.noreply.github.com>
2024-12-29 13:15:59 -0500

    Add some commands

--- LServer.sh
+++ LServer.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+cd ~/
+echo "cloning https://github.com/gitxpresso/clodsh"
+git clone https://github.com/gitxpresso/clodsh
+echo "moving to the LinuxServer Directory in the newly cloned repository"
+cd clodsh/LinuxServer          
